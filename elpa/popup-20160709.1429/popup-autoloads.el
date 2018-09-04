d4c523834b3312197022ef9aa86365f398d5f9b1
masyagin1998 <masyagin1998@yandex.ru>
2018-09-04 12:39:31 +0300

    Now you don't need elpa dir

--- elpa/popup-20160709.1429/popup-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; popup-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("popup.el") (23434 37002 206474 750000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; popup-autoloads.el ends here
--- elpa/yasnippet-classic-snippets-1.0.2/yasnippet-classic-snippets-autoloads.el
+++
@@ -1,23 +0,0 @@
-;;; yasnippet-classic-snippets-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "yasnippet-classic-snippets" "yasnippet-classic-snippets.el"
-;;;;;;  (23349 59300 555643 279000))
-;;; Generated autoloads from yasnippet-classic-snippets.el
- (eval-after-load 'yasnippet '(require 'yasnippet-classic-snippets))
-
-;;;***
-
-;;;### (autoloads nil nil ("yasnippet-classic-snippets-pkg.el") (23349
-;;;;;;  59297 347608 498000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; yasnippet-classic-snippets-autoloads.el ends here
